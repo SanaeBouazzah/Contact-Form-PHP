063e88f3194a86d88b9b9c36eb38b6760340a0f7
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-03-05 03:28:41 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+</head>
+<body>
+  <div class="wrapper">
+    <header>Contact Us</header>
+    <form action="" method="POST">
+      <div class="fields">
+        <div class="field">
+          <input type="text" placeholder="Enter your name..." name="name">
+        </div>
+        <div class="field">
+          <input type="text" placeholder="Enter your email..."  name="email">
+        </div>
+      </div>
+      <div class="fields">
+        <div class="field">
+          <input type="text" placeholder="Enter your phone..." name="phone">
+        </div>
+        <div class="field">
+          <input type="text" placeholder="Enter your website..." name="website">
+        </div>
+      </div>
+      <div class="message">
+        <textarea name="message" placeholder="Write your message..."></textarea>
+      </div>
+      <div class="button-area">
+        <input type="submit" value="Send Message">
+        <span>Sending your message</span>
+      </div>
+    </form>
+  </div>
+
+
+  <script src="js/script.js"></script>
+</body>
+</html>
